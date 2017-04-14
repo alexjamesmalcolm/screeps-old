5f36d609580a959eee7e2a4fbe6bf8c2220c10ab
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 05:41:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,26 @@ module.exports.loop = function () {
             console.log('Clearing non-existing creep memory:', name);
         }
     }
+    
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    console.log('Harvesters: ' + harvesters.length);
+    console.log('Upgraders: ' + upgraders.length);
+    console.log('Builders: ' + builders.length);
+    if(harvesters.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    if(upgraders.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    if(builders.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
