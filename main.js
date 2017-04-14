dfa2672c6fdfd0d6fcf3301e72321709a3cf40ec
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 12:54:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -17,15 +17,13 @@ module.exports.loop = function () {
     console.log('Harvesters: ' + harvesters.length);
     console.log('Upgraders: ' + upgraders.length);
     console.log('Builders: ' + builders.length);
-    if(harvesters.length < 2) {
+    if(harvesters.length < 3) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
-    }
-    if(upgraders.length < 1) {
+    } else if(upgraders.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
-    }
-    if(builders.length < 1) {
+    } else if(builders.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
