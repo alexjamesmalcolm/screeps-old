1e122c53e733856eef3c852b95b6910d286834a3
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 05:37:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ var roleBuilder = require('role.builder');
 //var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
+    
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
