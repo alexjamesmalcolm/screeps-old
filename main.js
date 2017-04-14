3e9720422ea756d78df2de329904c8c8c0fcfec3
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 12:50:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -35,9 +35,10 @@ module.exports.loop = function () {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
+        } else if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
+        } else if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
         }
     }
 }
