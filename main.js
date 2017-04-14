82b1c8fb64e80baa3641a2719be75506e10aeb8b
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 04:36:54 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
