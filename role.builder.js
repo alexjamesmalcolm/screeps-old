015204faded70a4e07ed39ebe356ce607ae3c846
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 18:13:40 -0400

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,31 +1,39 @@
 var roleBuilder = {
-
-    /** @param {Creep} creep **/
     run: function(creep) {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-            creep.say('🔄 harvest');
+            creep.say('harvesting');
 	    }
 	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
-	        creep.say('🚧 build');
+	        creep.say('building');
 	    }
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    if(creep.memory.path == false) {
+                        creep.memory.path = creep.room.findPath(creep.pos, targets[0].pos);
+                    }
+                    creep.moveByPath(creep.memory.path);
+                    //creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                } else {
+                    creep.memory.path = false;
                 }
             }
 	    } else {
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                if(creep.memory.path == false) {
+                    creep.memory.path = creep.room.findPath(creep.pos, sources[0].pos);
+                }
+                creep.moveByPath(creep.memory.path);
+            } else {
+                creep.memory.path = false;
             }
 	    }
 	}
 };
-
 module.exports = roleBuilder;
