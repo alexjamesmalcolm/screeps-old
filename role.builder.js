0292db63d1412b56da3b74dcc077756346c8d250
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 15:26:39 -0400

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -32,6 +32,18 @@ var roleBuilder = {
                     */
                 }
             }
+            var repairTargets = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+                filter: function(object) {
+                    return object.hits < object.hitsMax
+                    && object.hitsMax - object.hits > REPAIR_POWER;
+                }
+            });
+            repairTargets.sort(function (a,b) {
+                return ((a.hits / a.hitsMax) - (b.hits / b.hitsMax))
+            });
+            if(repairTargets.length > 0) {
+                creep.repair(repairTargets[0]);
+            }
 	    } else {
 	        //var sources = creep.room.find(FIND_SOURCES);
             var source = creep.pos.findClosestByRange(FIND_SOURCES);
