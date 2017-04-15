518883c70c466ee95db13f41d6fe29e715b44ebe
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 12:22:15 -0400

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -34,7 +34,7 @@ var roleBuilder = {
             }
 	    } else {
 	        //var sources = creep.room.find(FIND_SOURCES);
-            var source = creep.pos.findClosestByRange(FIND_SOURCE);
+            var source = creep.pos.findClosestByRange(FIND_SOURCES);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
                 /*
