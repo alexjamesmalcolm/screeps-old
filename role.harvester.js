f17e490d2f0467c300a9ff0631595b56f0ff40f5
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 01:39:23 -0400

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,10 +1,12 @@
 var roleHarvester = {
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
+	        creep.say('Harvesting');
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 if(creep.memory.path == false) {
                     creep.memory.path = creep.room.findPath(creep.pos, sources[0].pos);
+                    creep.say('Pathing');
                 }
                 if(creep.moveByPath(creep.memory.path) == ERR_NOT_FOUND) {
                     creep.memory.path = false;
@@ -12,6 +14,7 @@ var roleHarvester = {
                 }
             } else {
                 creep.memory.path = false;
+                creep.say('Pausing');
             }
         } else {
             var targets = creep.room.find(FIND_STRUCTURES, {
@@ -29,8 +32,13 @@ var roleHarvester = {
                         creep.memory.path = creep.room.findPath(creep.pos, targets[0].pos);
                     }
                     creep.moveByPath(creep.memory.path);
+                    if(creep.moveByPath(creep.memory.path) == ERR_NOT_FOUND) {
+                        creep.memory.path = false;
+                        creep.say('Pausing');
+                    }
                 } else {
                     creep.memory.path = false;
+                    creep.say('Pausing');
                 }
             }
         }
