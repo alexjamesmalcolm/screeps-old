8613c6208951861d2d19778b8a93f25d2eb7e640
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-14 21:16:09 -0400

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -3,14 +3,36 @@ var roleHarvester = {
 	    if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                if(creep.memory.path == false) {
+                    creep.memory.path = creep.room.findPath(creep.pos, sources[0].pos);
+                }
+                creep.moveByPath(creep.memory.path);
+            } else {
+                creep.memory.path = false;
             }
         } else {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: function(structure) {
+                    if(structure.structureType == STRUCTURE_EXTENSION) {
+                        return structure.energy < structure.energyCapacity;
+                    } else if (structure.structureType == STRUCTURE_SPAWN) {
+                        return structure.energy < structure.energyCapacity;
+                    }
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+	    	        if(creep.memory.path == false) {
+                        creep.memory.path = creep.room.findPath(creep.pos, targets[0].pos);
+                    }
+                    creep.moveByPath(creep.memory.path);
+                } else {
+                    creep.memory.path = false;
+                }
             }
         }
 	}
 };
+
 
 module.exports = roleHarvester;
