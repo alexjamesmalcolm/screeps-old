ae4a9edad4f3509fc6f713f58612ea85211a2599
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 16:09:22 -0400

    Update role.courier.js

--- role.courier.js
+++ role.courier.js
@@ -1,7 +1,34 @@
 var roleCourier = {
     run: function(creep) {
         /*It's got to find storage and containers*/
-        
+        if(creep.memory.collecting) {
+            if(creep.carry.energy == creep.carryCapacity) {
+                creep.memory.collecting = false;
+                creep.say('Delivering');
+            }
+        } else {
+            if(creep.carry.energy == 0) {
+                creep.memory.collecting = true;
+                creep.say('Collecting');
+            }
+        }
+        if(creep.memory.collecting) {
+        } else {
+            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: function(structure) {
+                    if(structure.structureType == STRUCTURE_EXTENSION) {
+                        return structure.energy < structure.energyCapacity;
+                    } else if(structure.structureType == STRUCTURE_SPAWN) {
+                        return structure.energy < structure.energyCapacity;
+                    }
+                }
+            });
+            if(target) {
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        }
     }
 };
 
