69864da01533e896a8e13f1bf777e26fe01d21dd
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 15:43:46 -0400

    Update state.dying.js

--- state.dying.js
+++ state.dying.js
@@ -10,6 +10,7 @@ var stateDying = {
         }
         creep.say('Dying');
         var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+        console.log(spawn.renewCreep(creep));
         if(spawn.renewCreep(creep) == ERR_NOT_IN_RANGE) {
             creep.moveTo(spawn);
         }
