e2ce49f591583a78fc487b1767e5431cdaf6a064
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 00:48:08 -0400

    Update role.fighter.js

--- role.fighter.js
+++ role.fighter.js
@@ -1,6 +1,11 @@
 var roleFighter = {
     run: function(creep) {
-        
+        var enemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+        if(enemy) {
+            if(creep.attack(enemy) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
     }
 };
 
