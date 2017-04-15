47746db306379d4584a32799b269a28a289c4d8c
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 12:49:30 -0400

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,11 +1,22 @@
 var roleHarvester = {
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            creep.say('Harvesting');
+        if(creep.memory.harvesting) {
+            if(creep.carry.energy == creep.carryCapacity) {
+                creep.memory.harvesting = false;
+                creep.say('Carrying');
+            }
+        } else {
+            if(creep.carry.energy == 0) {
+                creep.memory.harvesting = true;
+                creep.say('Harvesting');
+            }
+        }
+        if(creep.memory.harvesting) {
             var source = creep.pos.findClosestByPath(FIND_SOURCES); //Inefficient
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
-                /*
+            }
+            /*
                 if(creep.memory.path == false) {
                     creep.memory.path = creep.room.findPath(creep.pos, source.pos);
                     creep.say('Pathing');
@@ -18,21 +29,25 @@ var roleHarvester = {
                 creep.memory.path = false;
                 creep.say('Pausing');
                 */
-            }
         } else {
             var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: function(structure) {
+                    console.log(structure.structureType);
                     if(structure.structureType == STRUCTURE_EXTENSION) {
                         return structure.energy < structure.energyCapacity;
-                    } else if (structure.structureType == STRUCTURE_SPAWN) {
+                    } else if(structure.structureType == STRUCTURE_SPAWN) {
                         return structure.energy < structure.energyCapacity;
+                    } else if(structure.structureType == STRUCTURE_CONTAINER) {
+                        return structure.energy < structure.energyCapacity;
+                    } else if(structure.structureType == STRUCTURE_STORAGE) {
+                        return structure.energy < strucutre.energyCapacity;
                     }
                 }
             });
             if(target) {
                 if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
-                    /*
+                                        /*
 	    	        if(creep.memory.path == false) {
                         creep.memory.path = creep.room.findPath(creep.pos, target.pos);
                     }
@@ -48,7 +63,6 @@ var roleHarvester = {
                 }
             }
         }
-        /*
         var repairTargets = creep.pos.findInRange(FIND_STRUCTURES, 1, {
             filter: function(object) {
                 return object.hits < object.hitsMax
@@ -61,7 +75,7 @@ var roleHarvester = {
         if(repairTargets.length > 0) {
             creep.repair(repairTargets[0]);
         }
-        */
+        
         creep.memory.lastPos = creep.pos;
     }
 };
