8657e0606d8cb3d5b2550fce5006cd1e120967a5
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 03:29:10 -0400

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -2,7 +2,7 @@ var roleHarvester = {
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
 	        creep.say('Harvesting');
-            var source = creep.pos.findClosestByRange(FIND_SOURCES);
+            var source = creep.pos.findClosestByPath(FIND_SOURCES); //Inefficient
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 if(creep.memory.path == false) {
                     creep.memory.path = creep.room.findPath(creep.pos, source.pos);
