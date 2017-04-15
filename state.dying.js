f530f3539f53dbba6f56a9cf0f583acbb6749004
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-15 15:46:04 -0400

    Update state.dying.js

--- state.dying.js
+++ state.dying.js
@@ -3,14 +3,13 @@ var stateDying = {
         if(!creep.memory.sick) {
             console.log('Bug in sickness');
         }
-        if(creep.ticksToLive < 1490) {
-            creep.memory.sick = true;
-        } else {
-            creep.memory.sick = false;
-        }
+        creep.memory.sick = true;
         creep.say('Dying');
         var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
         console.log(spawn.renewCreep(creep));
+        if(spawn.renewCreep(creep) == ERR_FULL) {
+            creep.memory.sick = false;
+        }
         if(spawn.renewCreep(creep) == ERR_NOT_IN_RANGE) {
             creep.moveTo(spawn);
         }
