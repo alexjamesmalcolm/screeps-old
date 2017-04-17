ee0f000a59160326b7bb133cc2441670d636c6d4
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 05:14:37 -0400

    Update Creep.roleBuilder.js

--- Creep.roleBuilder.js
+++ Creep.roleBuilder.js
@@ -1,16 +1,13 @@
 var roleBuilder = function() {
-    var say = function(text) {
-        this.say('B: '+text);
-    }
     if(this.memory.building && this.carry.energy == 0) {
         this.memory.building = false;
         this.memory.needEnergy = true;
-        say('harvesting');
+        this.say('Need Energy');
     }
     if(!this.memory.building && this.carry.energy == this.carryCapacity) {
         this.memory.building = true;
         this.memory.needEnergy = false;
-        say('building');
+        this.say('Building');
     }
     if(this.memory.building) {
         var target = this.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
