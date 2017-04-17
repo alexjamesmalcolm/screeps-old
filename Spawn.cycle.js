d3a85d46681d54a391fe12d1371996aaac1c7a44
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 17:04:05 -0400

    Update Spawn.cycle.js

--- Spawn.cycle.js
+++ Spawn.cycle.js
@@ -8,18 +8,18 @@ var SpawnCycle = function() {
     var sick = _.filter(Game.creeps, (creep) => creep.memory.sick);
     //console.log("There are "+harvesters.length+" harvesters, "+upgraders.length+" upgraders, "+builders.length+" builders, "+fighters.length+" fighters, and "+sick.length+" sick");
     if(harvesters.length < creepCount['harvesters']) {
-        var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        var newName = this.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         //console.log('Spawning new harvester: ' + newName);
     } else if(couriers.length < creepCount['couriers']) {
-        var newName = spawn.createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'courier'});
+        var newName = this.createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'courier'});
     } else if(upgraders.length < creepCount['upgraders']) {
-        var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        var newName = this.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         //console.log('Spawning new upgrader: ' + newName);
     } else if(builders.length < creepCount['builders']) {
-        var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
+        var newName = this.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
         //console.log('Spawning new builder: ' + newName);
     } else if(fighters.length < creepCount['fighters']) {
-        var newName = spawn.createCreep([MOVE,MOVE,RANGED_ATTACK,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH], undefined, {role: 'fighter'});
+        var newName = this.createCreep([MOVE,MOVE,RANGED_ATTACK,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH], undefined, {role: 'fighter'});
         //console.log('Spawning new fighter: ' + newName);
     }
 };
