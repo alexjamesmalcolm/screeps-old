896c8ab470e8399c724aec3e4437585ecb321bbd
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-16 22:06:07 -0400

    Update state.recycle.js

--- state.recycle.js
+++ state.recycle.js
@@ -1,6 +1,13 @@
 var stateRecycle = {
     run: function(creep) {
-        var spawn
+        if(creep.memory.recycle) {
+            var spawn = creep.memory.recycle.nearestSpawn;
+            if()
+        } else {
+            creep.memory.recycle = {
+                nearestSpawn: creep.pos.findClosestByPath(STRUCTURE_SPAWN);
+            };
+        }
     }
 };
 
