102c50d3ed7033a795fff131f73a139c221f214d
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 03:58:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -35,18 +35,18 @@ module.exports.loop = function () {
         var fighters = _.filter(Game.creeps, (creep) => creep.memory.role == 'fighter');
         var sick = _.filter(Game.creeps, (creep) => creep.memory.sick);
         console.log("There are "+harvesters.length+" harvesters, "+upgraders.length+" upgraders, "+builders.length+" builders, "+fighters.length+" fighters, and "+sick.length+" sick");
-        if(harvesters.length < creepCount['harvesters']) {
+        if(harvesters.length < Creep.creepCount['harvesters']) {
             var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
             //console.log('Spawning new harvester: ' + newName);
-        } else if(couriers.length < creepCount['couriers']) {
+        } else if(couriers.length < Creep.creepCount['couriers']) {
             var newName = spawn.createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'courier'});
-        } else if(upgraders.length < creepCount['upgraders']) {
+        } else if(upgraders.length < Creep.creepCount['upgraders']) {
             var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
             //console.log('Spawning new upgrader: ' + newName);
-        } else if(builders.length < creepCount['builders']) {
+        } else if(builders.length < Creep.creepCount['builders']) {
             var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
             //console.log('Spawning new builder: ' + newName);
-        } else if(fighters.length < creepCount['fighters']) {
+        } else if(fighters.length < Creep.creepCount['fighters']) {
             var newName = spawn.createCreep([MOVE,MOVE,RANGED_ATTACK,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH], undefined, {role: 'fighter'});
             //console.log('Spawning new fighter: ' + newName);
         }
