a46d28c1ef68dc901df3a58e0311ed345abceec7
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 17:45:44 -0400

    Update Room.cycle.js

--- Room.cycle.js
+++ Room.cycle.js
@@ -4,6 +4,11 @@ var RoomCycle = function() {
         var spawn = Game.spawns[name];
         spawn.cycle();
     }
+    var creeps = this.find(FIND_MY_CREEPS);
+    for(var name in creeps) {
+        var creep = Game.creeps[name];
+        creep.cycle();
+    }
 };
 
 module.exports = RoomCycle;
