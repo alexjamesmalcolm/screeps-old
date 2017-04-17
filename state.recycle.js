0ce154ff595ebcb0e6499579a9e0712c852aac84
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-16 22:14:58 -0400

    Update state.recycle.js

--- state.recycle.js
+++ state.recycle.js
@@ -1,12 +1,35 @@
 var stateRecycle = {
     run: function(creep) {
         if(creep.memory.recycle) {
-            var spawn = creep.memory.recycle.nearestSpawn;
-            //if()
+            var spawn = creep.memory.nearestSpawn;
+            if(creep.carry.energy > 0) {
+                var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: function(structure) {
+                        if(structure.structureType == STRUCTURE_EXTENSION) {
+                            return structure.energy < structure.energyCapacity;
+                        } else if(structure.structureType == STRUCTURE_SPAWN) {
+                            return structure.energy < structure.energyCapacity;
+                        } else if(structure.structureType == STRUCTURE_CONTAINER) {
+                            return _.sum(structure.store) < structure.storeCapacity
+                        } else if(structure.structureType == STRUCTURE_STORAGE) {
+                            return structure.energy < strucutre.energyCapacity; //Incorrect, should be similar to container
+                        }
+                    }
+                });
+                if(target) {
+                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                }
+            } else {
+                if(spawn.recycleCreep(creep) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(spawn);
+                    creep.say('Recycling');
+                }
+            }
         } else {
-            creep.memory.recycle = {
-                nearestSpawn: creep.pos.findClosestByPath(STRUCTURE_SPAWN)
-            };
+            creep.memory.recycle = true;
+            creep.memory.nearestSpawn = creep.pos.findClosestByPath(STRUCTURE_SPAWN);
         }
     }
 };
