2496c78ed82262776f4b3f978090a4af5808e2f8
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 15:15:46 -0400

    Update Creep.activeRepair.js

--- Creep.activeRepair.js
+++ Creep.activeRepair.js
@@ -1,3 +1,39 @@
-var CreepActiveRepair = function(input) {};
+var CreepActiveRepair = function(input) {
+    var creep = this;
+    var constructionSite = this.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+    var repairTarget = this.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: function(structure) {
+            if(structure.hits < structure.hitsMax) {
+                if(structure.hitsMax - structure.hits > REPAIR_POWER * creep.getActiveBodyparts(WORK)) {
+                    if(structure.structureType == STRUCTURE_RAMPART) {
+                        return false;
+                    } else if(structure.structureType == STRUCTURE_CONTAINER) {
+                        return true;
+                    } else if(structure.structureType == STRUCTURE_STORAGE) {
+                        return true;
+                    }
+                }
+            }
+        }
+    });
+    var closestConstructionSiteDistance = this.pos.getRangeTo(constructionSite);
+    var closestRepairTargetDistance = this.pos.getRangeTo(repairTarget);
+    var target;
+    if (closestConstructionSiteDistance <= closestRepairTargetDistance) {
+        target = constructionSite;
+        if(target) {
+            if(this.build(target) == ERR_NOT_IN_RANGE) {
+                this.moveTo(target);
+            }
+        }
+    } else {
+        target = repairTarget;
+        if(target) {
+            if(this.repair(target) == ERR_NOT_IN_RANGE) {
+                this.moveTo(target);
+            }
+        }
+    }
+};
 
 module.exports = CreepActiveRepair;
