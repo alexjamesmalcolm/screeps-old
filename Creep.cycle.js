87089eb04779e5f4c4adcb9a144aceb8cdbd2e19
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 03:53:56 -0400

    Update Creep.cycle.js

--- Creep.cycle.js
+++ Creep.cycle.js
@@ -1,1 +1,29 @@
+var CreepCycle = function() {
+    var sick = _.filter(Game.creeps, (creep) => creep.memory.sick);
+    if(!this.memory.sick) {
+        if(sick < creepCount.sick) {
+            if(this.ticksToLive < 500) {
+                this.memory.sick = true;
+            }
+        } else {
+            this.memory.sick = false;
+        }
+    }
+    if(this.memory.sick) {
+        stateDying.run(this);
+    } else if(this.memory.recycle) {
+        stateRecycle.run(this);
+    } else if(this.memory.role == 'harvester') {
+        roleHarvester.run(this);
+    } else if(this.memory.role == 'builder') {
+        roleBuilder.run(this);
+    } else if(this.memory.role == 'upgrader') {
+        roleUpgrader.run(this);
+    } else if(this.memory.role == 'fighter') {
+        roleFighter.run(this);
+    } else if(this.memory.role == 'courier') {
+        roleCourier.run(this);
+    }
+};
 
+module.exports = CreepCycle;
