903495fa2fef9ca44aef6f4c9f81305462f666ef
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 17:05:00 -0400

    Update Creep.collect.js

--- Creep.collect.js
+++ Creep.collect.js
@@ -18,7 +18,7 @@ var CreepCollect = function(input) {
     var droppedEnergy = this.pos.findClosestByRange(FIND_DROPPED_ENERGY);
     if(droppedEnergy) {
         if(this.pickup(droppedEnergy) == ERR_NOT_IN_RANGE) {
-            this.moveTo(target);
+            this.moveTo(droppedEnergy);
         }
     } else if(target) {
         if(this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
