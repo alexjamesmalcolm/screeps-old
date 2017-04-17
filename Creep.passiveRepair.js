fb9bb1bbd5317328d5730f9d7584997d2fa244b1
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 02:06:31 -0400

    Update Creep.passiveRepair.js

--- Creep.passiveRepair.js
+++ Creep.passiveRepair.js
@@ -1,15 +1,16 @@
 var CreepPassiveRepair = function() {
-    var repairTargets = this.pos.findInRange(FIND_STRUCTURES, 1, {
+    var creep = this;
+    var repairTargets = creep.pos.findInRange(FIND_STRUCTURES, 1, {
         filter: function(structure) {
             return structure.hits < object.hitsMax
-            && object.hitsMax - object.hits > REPAIR_POWER * this.getActiveBodyparts(WORK);
+            && object.hitsMax - object.hits > REPAIR_POWER * creep.getActiveBodyparts(WORK);
         }
     });
     repairTargets.sort(function(a, b) {
         return (a.hits / a.hitsMax) - (b.hits / b.hitsMax);
     });
     if(repairTargets.length > 0) {
-        this.repair(repairTargets[0]);
+        creep.repair(repairTargets[0]);
     }
 };
 
