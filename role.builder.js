d633ceaa6993abf080ef7b8ec12b25955d49a29a
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 02:46:43 -0400

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -5,10 +5,12 @@ var roleBuilder = {
         }
         if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
+            creep.memory.needEnergy = true;
             say('harvesting');
         }
         if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
             creep.memory.building = true;
+            creep.memory.needEnergy = false;
             say('building');
         }
         
@@ -21,20 +23,6 @@ var roleBuilder = {
                 }
             }
             creep.passiveRepair();
-            /*
-            var repairTargets = creep.pos.findInRange(FIND_STRUCTURES, 1, {
-                filter: function(object) {
-                    return object.hits < object.hitsMax
-                    && object.hitsMax - object.hits > REPAIR_POWER;
-                }
-            });
-            repairTargets.sort(function (a,b) {
-                return ((a.hits / a.hitsMax) - (b.hits / b.hitsMax))
-            });
-            if(repairTargets.length > 0) {
-                creep.repair(repairTargets[0]);
-            }
-            */
         } else {
             creep.collect({
                 resource: RESOURCE_ENERGY,
