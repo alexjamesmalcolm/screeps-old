e5f335c4580d310b2293046bae649dfeb5dc7d5a
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 19:23:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var creepCount = require('creepCount');
 Creep.prototype.collect = require('Creep.collect');
 Creep.prototype.cycle = require('Creep.cycle');
+Creep.prototype.deposit = require('Creep.deposit');
 Creep.prototype.maintainBuild = require('Creep.maintainBuild');
 Creep.prototype.passiveRepair = require('Creep.passiveRepair');
 Creep.prototype.roleBuilder = require('Creep.roleBuilder');
