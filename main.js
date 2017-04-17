d5f0011fef7559ee410e45d8fd0d6930eba72b54
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 04:04:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var stateRecycle = require('state.recycle');
 Creep.prototype.cycle = require('Creep.cycle');
 Creep.prototype.collect = require('Creep.collect');
 Creep.prototype.passiveRepair = require('Creep.passiveRepair');
-Creep.prototype.creepCount = {
+var creepCount = {
     harvesters: 6,
     upgraders: 6,
     builders: 1,
@@ -35,18 +35,18 @@ module.exports.loop = function () {
         var fighters = _.filter(Game.creeps, (creep) => creep.memory.role == 'fighter');
         var sick = _.filter(Game.creeps, (creep) => creep.memory.sick);
         console.log("There are "+harvesters.length+" harvesters, "+upgraders.length+" upgraders, "+builders.length+" builders, "+fighters.length+" fighters, and "+sick.length+" sick");
-        if(harvesters.length < Creep.creepCount['harvesters']) {
+        if(harvesters.length < creepCount['harvesters']) {
             var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
             //console.log('Spawning new harvester: ' + newName);
-        } else if(couriers.length < Creep.creepCount['couriers']) {
+        } else if(couriers.length < creepCount['couriers']) {
             var newName = spawn.createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'courier'});
-        } else if(upgraders.length < Creep.creepCount['upgraders']) {
+        } else if(upgraders.length < creepCount['upgraders']) {
             var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
             //console.log('Spawning new upgrader: ' + newName);
-        } else if(builders.length < Creep.creepCount['builders']) {
+        } else if(builders.length < creepCount['builders']) {
             var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
             //console.log('Spawning new builder: ' + newName);
-        } else if(fighters.length < Creep.creepCount['fighters']) {
+        } else if(fighters.length < creepCount['fighters']) {
             var newName = spawn.createCreep([MOVE,MOVE,RANGED_ATTACK,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH], undefined, {role: 'fighter'});
             //console.log('Spawning new fighter: ' + newName);
         }
@@ -55,6 +55,6 @@ module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        creep.cycle();
+        creep.cycle(creepCount);
     }
 }
