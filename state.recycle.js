8097b3521bd03e2f17c759f47e3eb6fafb1030d9
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-16 22:06:35 -0400

    Update state.recycle.js

--- state.recycle.js
+++ state.recycle.js
@@ -2,7 +2,7 @@ var stateRecycle = {
     run: function(creep) {
         if(creep.memory.recycle) {
             var spawn = creep.memory.recycle.nearestSpawn;
-            if()
+            //if()
         } else {
             creep.memory.recycle = {
                 nearestSpawn: creep.pos.findClosestByPath(STRUCTURE_SPAWN);
