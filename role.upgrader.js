0a88fcab32eedd9fe239ffe6c9ca3be95ab236a6
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-17 02:02:17 -0400

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,6 +1,4 @@
 var roleUpgrader = {
-
-    /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
