9a276aec2bd8dd5d8eb990b7c5854327e7533260
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-18 04:40:41 -0400

    Update Room.cycle.js

--- Room.cycle.js
+++ Room.cycle.js
@@ -1,5 +1,57 @@
 var creepCount = require('creepCount');
+var harvesterSpawn = function(room) {
+    var sources = room.find(FIND_SOURCES);
+    room = Game.rooms['W93S6'];
+    var harvesters = room.find(FIND_MY_CREEPS, {
+        filter: function(creep) {
+            if(creep.memory.recycle) {
+                return false;
+            } else if(creep.memory.role == 'harvester') {
+                return true;
+            }
+        }
+    });
+    harvesters.sort(function(a, b) {
+        return a.getActiveBodyparts(WORK) > b.getActiveBodyparts(WORK);
+    });
+    var workBodyParts = Math.floor(-(BODYPART_COST[CARRY] + BODYPART_COST[MOVE] - room.energyAvailable) / BODYPART_COST[WORK]);
+    if(workBodyParts > 6) {
+        workBodyParts = 6;
+    }
+    var moveBodyParts = Math.floor((room.energyAvailable - BODYPART_COST[WORK] * workBodyParts - BODYPART_COST[CARRY]) / BODYPART_COST[MOVE]);
+    if(moveBodyParts > workBodyParts / 3) {
+        moveBodyParts = workBodyParts / 3;
+    }
+    var creepCost = moveBodyParts * BODYPART_COST[MOVE] + BODYPART_COST[CARRY] + workBodyParts * BODYPART_COST[WORK];
+    var bodyparts = [CARRY];
+    for(var i = 0; i < workBodyParts; i++) {
+        bodyparts.push(WORK);
+    }
+    for(var i = 0; i < moveBodyParts; i++) {
+        bodyparts.push(MOVE);
+    }
+    console.log(bodyparts);
+    if(harvesters.length > sources.length) {
+        harvesters[0].memory.recycle = true;
+    } else if(workBodyParts > harvesters[0].getActiveBodyparts(WORK)) {
+        var spawn = room.find(FIND_MY_SPAWNS, {
+            filter: function(spawn) {
+                if(spawn.spawning) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+        if(spawn) {
+            harvesters[0].memory.recycle = true;
+            spawn.createCreep(bodyparts, undefined, {role: 'harvester'});
+        }
+    }
+    
+};
 var RoomCycle = function() {
+    harvesterSpawn(this);
     var spawns = this.find(FIND_MY_SPAWNS);
     for(var name in spawns) {
         var spawn = spawns[name];
