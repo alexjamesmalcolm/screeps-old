4aa1e0b0ca29b8a7c3d030728b2faf5314b89f4c
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-19 03:02:19 -0400

    Update Room.cycle.js

--- Room.cycle.js
+++ Room.cycle.js
@@ -3,11 +3,23 @@ var RoomCycle = function() {
     this.harvesterSpawn();
     this.courierSpawn();
     this.upgraderSpawn();
+    var spawns = this.find(FIND_MY_SPAWNS, {
+        filter: function(spawn) {
+            if(spawn.spawning) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
+    this.memory.spawns = spawns;
+    /*
     var spawns = this.find(FIND_MY_SPAWNS);
     for(var name in spawns) {
         var spawn = spawns[name];
         //spawn.cycle();
     }
+    */
     var creeps = this.find(FIND_MY_CREEPS);
     for(var name in creeps) {
         var creep = creeps[name];
