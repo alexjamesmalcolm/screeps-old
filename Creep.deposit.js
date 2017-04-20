603db779ef5d5bd87073a3826e612ebf030adcc8
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-20 19:41:13 -0400

    Update Creep.deposit.js

--- Creep.deposit.js
+++ Creep.deposit.js
@@ -23,55 +23,61 @@ var CreepDeposit = function(input) {
 module.exports = CreepDeposit;
 
 /*
+//Collect Code
+this.collect({
+    resource: RESOURCE_ENERGY,
+    amount: this.carryCapacity,
+    structures: [STRUCTURE_CONTAINER, STRUCTURE_STORAGE]
+});
 //Courier Code
 var target;
-        var closestStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: function(structure) {
-                if (structure.structureType == STRUCTURE_EXTENSION) {
-                    return structure.energy < structure.energyCapacity;
-                } else if (structure.structureType == STRUCTURE_SPAWN) {
-                    return structure.energy < structure.energyCapacity;
-                }
-            }
-        });
-        var closestCreep = this.pos.findClosestByRange(FIND_CREEPS, {
-            filter: function(creep) {
-                if (creep.memory.needEnergy) {
-                    return true;
-                }
-            }
-        });
-        var closestStructureDistance = this.pos.getRangeTo(closestStructure);
-        var closestCreepDistance = this.pos.getRangeTo(closestCreep);
-        if (closestCreepDistance <= closestStructureDistance) {
-            target = closestCreep;
-        } else {
-            target = closestStructure
+var closestStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+    filter: function(structure) {
+        if (structure.structureType == STRUCTURE_EXTENSION) {
+            return structure.energy < structure.energyCapacity;
+        } else if (structure.structureType == STRUCTURE_SPAWN) {
+            return structure.energy < structure.energyCapacity;
         }
-        if (target) {
-            if (this.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                this.moveTo(target);
-            }
-        } else {
-            this.moveTo(Game.flags.Flag1);
+    }
+});
+var closestCreep = this.pos.findClosestByRange(FIND_CREEPS, {
+    filter: function(creep) {
+        if (creep.memory.needEnergy) {
+            return true;
         }
+    }
+});
+var closestStructureDistance = this.pos.getRangeTo(closestStructure);
+var closestCreepDistance = this.pos.getRangeTo(closestCreep);
+if (closestCreepDistance <= closestStructureDistance) {
+    target = closestCreep;
+} else {
+    target = closestStructure
+}
+if(target) {
+    if (this.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        this.moveTo(target);
+    }
+} else {
+    this.moveTo(Game.flags.Flag1);
+}
 //Harvester Code
 var target = this.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: function(structure) {
-                if (structure.structureType == STRUCTURE_EXTENSION) {
-                    return structure.energy < structure.energyCapacity;
-                } else if (structure.structureType == STRUCTURE_SPAWN) {
-                    return structure.energy < structure.energyCapacity;
-                } else if (structure.structureType == STRUCTURE_CONTAINER) {
-                    return _.sum(structure.store) < structure.storeCapacity
-                } else if (structure.structureType == STRUCTURE_STORAGE) {
-                    return structure.energy < strucutre.energyCapacity; //Incorrect, should be similar to container
-                }
-            }
-        });
-        if (target) {
-            if (this.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                this.moveTo(target);
-            }
+    filter: function(structure) {
+        if (structure.structureType == STRUCTURE_EXTENSION) {
+            return structure.energy < structure.energyCapacity;
+        } else if (structure.structureType == STRUCTURE_SPAWN) {
+            return structure.energy < structure.energyCapacity;
+        } else if (structure.structureType == STRUCTURE_CONTAINER) {
+            return _.sum(structure.store) < structure.storeCapacity
+        } else if (structure.structureType == STRUCTURE_STORAGE) {
+            return structure.energy < strucutre.energyCapacity; //Incorrect, should be similar to container
         }
+    }
+});
+if (target) {
+    if (this.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        this.moveTo(target);
+    }
+}
 */
