68f22d2f1cc2685145dd5f45c15e5964050f462c
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-20 19:35:42 -0400

    Update Creep.deposit.js

--- Creep.deposit.js
+++ Creep.deposit.js
@@ -23,6 +23,7 @@ var CreepDeposit = function(input) {
 module.exports = CreepDeposit;
 
 /*
+//Courier Code
 var target;
         var closestStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: function(structure) {
@@ -53,5 +54,24 @@ var target;
             }
         } else {
             this.moveTo(Game.flags.Flag1);
+        }
+//Harvester Code
+var target = this.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: function(structure) {
+                if (structure.structureType == STRUCTURE_EXTENSION) {
+                    return structure.energy < structure.energyCapacity;
+                } else if (structure.structureType == STRUCTURE_SPAWN) {
+                    return structure.energy < structure.energyCapacity;
+                } else if (structure.structureType == STRUCTURE_CONTAINER) {
+                    return _.sum(structure.store) < structure.storeCapacity
+                } else if (structure.structureType == STRUCTURE_STORAGE) {
+                    return structure.energy < strucutre.energyCapacity; //Incorrect, should be similar to container
+                }
+            }
+        });
+        if (target) {
+            if (this.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(target);
+            }
         }
 */
