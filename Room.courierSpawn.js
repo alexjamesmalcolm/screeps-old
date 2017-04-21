329356edea1c8cdcffc87ec66225f224cd1c2e4f
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-21 01:44:25 -0400

    Update Room.courierSpawn.js

--- Room.courierSpawn.js
+++ Room.courierSpawn.js
@@ -39,25 +39,34 @@ var RoomCourierSpawn = function() {
     });
     var courier = optimalCourier(this);
     var totalCarry = 0;
-    var totalCarryWithoutLeastCreep = 0;
     couriers.forEach(function(creep, i) {
         totalCarry = totalCarry + CARRY_CAPACITY * creep.getActiveBodyparts(CARRY);
-        if(i != 0) {
-            totalCarryWithoutLeastCreep = totalCarryWithoutLeastCreep + CARRY_CAPACITY * creep.getActiveBodyparts(CARRY);
-        }
     });
-    if(totalCarryWithoutLeastCreep > this.memory.harvestPerTick * 10) {
-        couriers[0].memory.recycle = true;
-    } else if(totalCarry + CARRY_CAPACITY * courier.carryBodyparts < this.memory.harvestPerTick * 10) {
-        if(this.memory.spawns.length > 0) {
-            this.memory.spawns[0].createCreep(courier.bodyparts, undefined, {role: 'courier'});
+    if(couriers.length > 0) {
+        if(totalCarry - CARRY_CAPACITY * couriers[0].getActiveBodyparts(WORK) > this.memory.harvestPerTick * 10) {
+            if(this.memory.spawns.length > 0) {
+                this.memory.spawns[0].createCreep(courier.bodyparts, undefined, {role: 'courier'});
+            }
         }
-    } else if(couriers.length > 0) {
-        if(courier.carryBodyparts > couriers[0].getActiveBodyparts(CARRY)) {
-            if(Math.ceil(couriers[0].weight() / couriers[0].getActiveBodyparts(MOVE)) >= 1) {
-                if(this.memory.spawns.length > 0) {
+    }
+    if(this.memory.spawns.length > 0) {
+        if(courier) {
+            /*
+            console.log(JSON.stringify(courier));
+            console.log(totalCarry);
+            console.log(CARRY_CAPACITY * courier.carryBodyparts);
+            console.log(this.memory.harvestPerTick * 10);
+            */
+            if(totalCarry + CARRY_CAPACITY * courier.carryBodyparts < this.memory.harvestPerTick * 10) { //This needs work
+                this.memory.spawns[0].createCreep(courier.bodyparts, undefined, {role: 'courier'});
+            } else if(couriers.length > 0) {
+                if(totalCarry - CARRY_CAPACITY * couriers[0].getActiveBodyparts(CARRY) > this.memory.harvestPerTick * 10) { //This needs work
                     couriers[0].memory.recycle = true;
-                    this.memory.spawns[0].createCreep(courier.bodyparts, undefined, {role: 'courier'});
+                } else if(courier.carryBodyparts > couriers[0].getActiveBodyparts(CARRY)) {
+                    if(Math.ceil(couriers[0].weight() / couriers[0].getActiveBodyparts(MOVE)) >= 1) {
+                        couriers[0].memory.recycle = true;
+                        this.memory.spawns[0].createCreep(courier.bodyparts, undefined, {role: 'courier'});
+                    }
                 }
             }
         }
