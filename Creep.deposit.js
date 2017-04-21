bdef6ee268057bf74bdbd99610de9897c352e846
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-20 21:04:57 -0400

    Update Creep.deposit.js

--- Creep.deposit.js
+++ Creep.deposit.js
@@ -7,28 +7,32 @@ var CreepDeposit = function(input) {
         return structure.structureType == structureType
         && structures.indexOf(structureType) != -1;
     }
-    var closestStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: function(structure) {
-            if(check(structure, structures, STRUCTURE_EXTENSION)) {
-                return structure.energyCapacity - structure.energy >= amount;
-            } else if(check(structure, structures, STRUCTURE_SPAWN)) {
-                return structure.energyCapacity - structure.energy >= amount;
-            } else if(check(structure, structures, STRUCTURE_STORAGE)) {
-                return structure.storeCapacity - _.sum(structure.store) >= amount;
-            } else if(check(structure, structures, STRUCTURE_CONTAINER)) {
-                return structure.storeCapacity - _.sum(structure.store) >= amount;
+    if(structures.length > 0) {
+        var closestStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: function(structure) {
+                if(check(structure, structures, STRUCTURE_EXTENSION)) {
+                    return structure.energyCapacity - structure.energy >= amount;
+                } else if(check(structure, structures, STRUCTURE_SPAWN)) {
+                    return structure.energyCapacity - structure.energy >= amount;
+                } else if(check(structure, structures, STRUCTURE_STORAGE)) {
+                    return structure.storeCapacity - _.sum(structure.store) >= amount;
+                } else if(check(structure, structures, STRUCTURE_CONTAINER)) {
+                    return structure.storeCapacity - _.sum(structure.store) >= amount;
+                }
             }
-        }
-    });
-    var closestCreep = this.pos.findClosestByRange(FIND_MY_CREEPS, {
-        filter: function(creep) {
-            if(creep.memory.needEnergy) {
-                return true;
+        });
+        var closestStructureDistance = this.pos.getRangeTo(closestStructure);
+    }
+    if(creepDepositing) {
+        var closestCreep = this.pos.findClosestByRange(FIND_MY_CREEPS, {
+            filter: function(creep) {
+                if(creep.memory.needEnergy) {
+                    return true;
+                }
             }
-        }
-    });
-    var closestStructureDistance = this.pos.getRangeTo(closestStructure);
-    var closestCreepDistance = this.pos.getRangeTo(closestCreep);
+        });
+        var closestCreepDistance = this.pos.getRangeTo(closestCreep);
+    }
     if(closestCreepDistance <= closestStructureDistance) {
         target = closestCreep;
     } else {
