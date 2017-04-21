f23f04c9128bbb8f329ef8d58030ea4827006b5e
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-20 21:48:00 -0400

    Update state.recycle.js

--- state.recycle.js
+++ state.recycle.js
@@ -1,6 +1,7 @@
 var stateRecycle = {
     run: function(creep) {
         if(creep.memory.recycle) {
+            creep.memory.closestSpawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
             var spawn = creep.memory.closestSpawn;
             if(creep.carry.energy > 0) {
                 creep.deposit({
@@ -8,6 +9,8 @@ var stateRecycle = {
                     structures: [STRUCTURE_EXTENSION, STRUCTURE_SPAWN, STRUCTURE_CONTAINER, STRUCTURE_STORAGE]
                 });
             } else {
+                console.log(spawn);
+                console.log(creep.pos.findClosestByRange(FIND_MY_SPAWNS));
                 if(spawn.recycleCreep(creep) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(spawn);
                     creep.say('Recycling');
@@ -15,7 +18,6 @@ var stateRecycle = {
             }
         } else {
             creep.memory.recycle = true;
-            creep.memory.closestSpawn = creep.pos.findClosestByRange(STRUCTURE_SPAWN);
         }
     }
 };
