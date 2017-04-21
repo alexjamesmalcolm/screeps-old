1d3260bd0b0aa46f6fa193f76b56c05131b6ec52
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-21 02:38:06 -0400

    Update Creep.sick.js

--- Creep.sick.js
+++ Creep.sick.js
@@ -1,22 +1,16 @@
-var stateDying = {
-    run: function(creep) {
-        if(!creep.memory.sick) {
-            console.log('Bug in sickness');
-        }
-        creep.memory.sick = true;
-        var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
-        if(spawn.renewCreep(creep) == ERR_FULL) {
-            creep.memory.sick = false;
-        }
-        if(spawn.renewCreep(creep) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(spawn);
-            creep.say('Dying');
-        } else if(spawn.renewCreep(creep) == OK) {
-            creep.say('Healing');
-        } else {
-            creep.say('Dying');
-        }
+var CreepSick = function() {
+    var spawn = this.pos.findClosestByRange(FIND_MY_SPAWNS);
+    var result = spawn.renewCreep(this);
+    if(result == ERR_FULL) {
+        this.memory.sick = false;
+    } else if(result == ERR_NOT_IN_RANGE) {
+        this.moveTo(spawn);
+        this.say('Sick');
+    } else if(result == OK) {
+        this.say('Healing');
+    } else if(result == ERR_NOT_ENOUGH_ENERGY) {
+        this.memory.sick = false;
     }
 };
 
-module.exports = stateDying;
+module.exports = CreepSick;
