58b54d977723c315b2efd168e37849ac0b672a8d
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-23 19:02:24 -0400

    Update Creep.pathing.js

--- Creep.pathing.js
+++ Creep.pathing.js
@@ -1,5 +1,6 @@
 var CreepPathing = function(target) {
-    var path, start, landmarks, landmarksById, end;
+    var path, start, landmarks, landmarksById, end, moving;
+    moving = false;
     if(this.memory.path) {
         if(typeof this.memory.path == 'string') {
             path = Room.deserializePath(this.memory.path);
@@ -8,9 +9,6 @@ var CreepPathing = function(target) {
         }
         end = new RoomPosition(path[path.length-1].x, path[path.length-1].y, this.room.name);
         if(target.pos.x != end.x || target.pos.y != end.y) {
-            for(var i = 0; i < path.length; i++) {
-                console.log(JSON.stringify(path[i]));
-            }
             delete this.memory.path;
         }
     }
@@ -25,13 +23,20 @@ var CreepPathing = function(target) {
         } else {
             if(this.pos.isNearTo(start)) {
                 path = this.room.memory.paths[start.id][target.id];
+                if(path[0].x != this.pos.x || path[0].y != this.pos.y) {
+                    this.moveTo(path[0].x, path[0].y);
+                    moving = true;
+                }
             } else {
                 path = this.pos.findPathTo(start);
             }
         }
         this.memory.path = path;
     }
-    this.moveByPath(this.memory.path);
+    if(!moving) {
+        this.moveByPath(this.memory.path);
+    }
+    moving = true;
 };
 
 module.exports = CreepPathing;
