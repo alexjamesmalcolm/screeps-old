e8be89d7cc655153a1ee3d5ebe0b5f1f914ec80b
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-23 16:55:40 -0400

    Update Room.paths.js

--- Room.paths.js
+++ Room.paths.js
@@ -1,13 +1,22 @@
-function Path(room, start, end) {
-    this.start = start;
-    this.end = end;
-    this.path = room.findPath(start.pos, end.pos, {
-        serialize: true,
-        ignoreCreeps: true
-    });
-}
+var createPaths = function(room, landmarks) {
+    var paths, i, j, start, end;
+    paths = {};
+    for(i = 0; i < landmarks.length; i++) {
+        start = landmarks[i];
+        paths[start] = {};
+        for(j = 0; j < landmarks.length; j++) {
+            end = landmarks[j];
+            paths[start][end] = room.findPath(Game.getObjectById(start).pos, Game.getObjectById(end).pos, {
+                serialize: true,
+                ignoreCreeps: true
+            });
+        }
+    }
+    return paths;
+};
+
 var RoomPaths = function() {
-    var landmarks, structures, sources, spawns, controller, sites, paths, start, end;
+    var landmarks, structures, sources, spawns, controller, sites, paths, start, end, i, j;
     /*
     Paths should exist in the memory of the room. Paths should reference the object that they lead to. Paths should be checked and contain information concerning whether there is a hostile creep threatening the path. If the hostile creep is neutral then check if the path is actually in danger of agro. If the hostile creep is of another player then this problem should probably be handled by a different method.
     Paths should be saved in the memory of the room
@@ -15,7 +24,12 @@ var RoomPaths = function() {
     Paths should be catagorized as safe or unsafe.
     */
     landmarks = [];
-    structures = this.find(FIND_MY_STRUCTURES);
+    structures = this.find(FIND_MY_STRUCTURES, {
+        filter: function(structure) {
+            console.log(structure);
+            return true;
+        }
+    });
     sources = this.find(FIND_SOURCES);
     spawns = this.find(FIND_MY_SPAWNS);
     controller = [this.controller];
@@ -24,33 +38,30 @@ var RoomPaths = function() {
     landmarks = landmarks.map(function(object) {
         return object.id;
     });
-    if(this.memory.paths) {
-        /*
-        var landmarks = [];
-        var structures = this.find(FIND_MY_STRUCTURES);
-        var sources = this.find(FIND_SOURCES);
-        var spawns = this.find(FIND_MY_SPAWNS);
-        var controller = [this.controller];
-        var sites = this.find(FIND_CONSTRUCTION_SITES);
-        landmarks = landmarks.concat(structures, sources, spawns, controller, sites);
-        landmarks = landmarks.map(function(object) {
-            return object.id;
-        });
-        this.memory.landmarks = landmarks;
-        */
-        
-    } else {
-        //this.memory.landmarks = landmarks;
-        paths = {};
-        for(var i = 0; i < landmarks.length; i++) {
-            start = landmarks[i];
-            paths[start] = {};
-            for(var j = 0; j < landmarks.length; j++) {
-                end = landmarks[j];
-                paths[start][end] = this.findPath(Game.getObjectById(start).pos, Game.getObjectById(end).pos);
+    paths = this.memory.paths;
+    if(paths) {
+        var pathDoesNotExist = false;
+        for(i = 0; i < landmarks.length; i++) {
+            var landmarkId = landmarks[i];
+            if(!paths[landmarkId]) {
+                pathDoesNotExist = true;
             }
         }
-        this.memory.paths = paths;
+        if(pathDoesNotExist) {
+            this.memory.paths = createPaths(this, landmarks);
+        } else {
+            var landmarkDoesNotExist = false;
+            for(start in paths) {
+                if(landmarks.indexOf(start) == -1) {
+                    landmarkDoesNotExist = true;
+                }
+            }
+            if(landmarkDoesNotExist) {
+                this.memory.paths = createPaths(this, landmarks);
+            }
+        }
+    } else {
+        this.memory.paths = createPaths(this, landmarks);
     }
 };
 
