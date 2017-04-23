bd4b48dc8ee3588c29acb1fdf1fa93258f8650e7
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-23 03:43:12 -0400

    Update Room.paths.js

--- Room.paths.js
+++ Room.paths.js
@@ -1,5 +1,7 @@
 var RoomPaths = function() {
-    console.log(true);
+    /*
+    Paths should exist in the memory of the room. Paths should reference the object that they lead to. Paths should be checked and contain information concerning whether there is a hostile creep threatening the path. If the hostile creep is neutral then check if the path is actually in danger of agro. If the hostile creep is of another player then this problem should probably be handled by a different method.
+    */
 };
 
 module.exports = RoomPaths;
