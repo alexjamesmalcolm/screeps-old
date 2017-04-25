e3d02d6a2d190bd22defb54aeaf81dc14fef552a
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-25 04:16:28 -0400

    Update Creep.roleHarvester.js

--- Creep.roleHarvester.js
+++ Creep.roleHarvester.js
@@ -18,7 +18,7 @@ var roleHarvester = function() {
         }
     } else {
         try {this.deposit({
-            creepDepositing: true,
+            creepDepositing: false,
             structures: [STRUCTURE_CONTAINER, STRUCTURE_STORAGE, STRUCTURE_EXTENSION, STRUCTURE_SPAWN]
         });} catch(err) {console.log(err+": Creep.roleHarvester.js this.deposit()");}
         try {this.passiveRepair();} catch(err) {console.log(err+": Creep.roleHarvester.js this.passiveRepair()");}
