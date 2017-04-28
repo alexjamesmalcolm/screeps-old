cfa66b6f53bdb0e7654af168349517453952be92
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-28 01:27:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,61 @@
-var creepCount = require('creepCount');
 try {
     Creep.prototype.collect = require('Creep.collect');
     Creep.prototype.cycle = require('Creep.cycle');
     Creep.prototype.deposit = require('Creep.deposit');
     Creep.prototype.maintainBuild = require('Creep.maintainBuild');
     Creep.prototype.passiveRepair = require('Creep.passiveRepair');
+    Creep.prototype.pathing = require('Creep.pathing');
     Creep.prototype.roleBuilder = require('Creep.roleBuilder');
     Creep.prototype.roleCourier = require('Creep.roleCourier');
     Creep.prototype.roleFighter = require('Creep.roleFighter');
     Creep.prototype.roleHarvester = require('Creep.roleHarvester');
     Creep.prototype.roleUpgrader = require('Creep.roleUpgrader');
-    Creep.prototype.weight = require('Creep.weight');
     Creep.prototype.sick = require('Creep.sick');
-    Creep.prototype.pathing = require('Creep.pathing');
+    Creep.prototype.weight = require('Creep.weight');
+    Room.prototype.builderSpawn = require('Room.builderSpawn');
+    Room.prototype.courierSpawn = require('Room.courierSpawn');
     Room.prototype.cycle = require('Room.cycle');
     Room.prototype.harvesterSpawn = require('Room.harvesterSpawn');
-    Room.prototype.courierSpawn = require('Room.courierSpawn');
-    Room.prototype.upgraderSpawn = require('Room.upgraderSpawn');
-    Room.prototype.builderSpawn = require('Room.builderSpawn');
     Room.prototype.paths = require('Room.paths');
     Room.prototype.roadBuilder = require('Room.roadBuilder');
     Room.prototype.towers = require('Room.towers');
+    Room.prototype.upgraderSpawn = require('Room.upgraderSpawn');
     Spawn.prototype.cycle = require('Spawn.cycle');
 } catch(err) {console.log(err+": main.js 1");}
 const profiler = require('screeps-profiler');
 profiler.enable();
 module.exports.loop = function () {
     profiler.wrap(function() {
-        for(var name in Memory.creeps) {
-            var creep = Game.creeps[name];
-            if(creep) {
-                if(creep.ticksToLive <= 1) {
-                    for(var resourceType in creep.carry) {
-                        creep.drop(resourceType);
+        var name;
+        for(name in Memory.creeps) {
+            if(Memory.creeps[name]) {
+                var creep = Game.creeps[name];
+                if(creep) {
+                    if(creep.ticksToLive <= 1) {
+                        for(var resourceType in creep.carry) {
+                            if(creep.carry[resourceType]) {
+                                creep.drop(resourceType);
+                            }
+                        }
+                        try {creep.suicide();} catch(err) {console.log(err+": main.js creep.suicide()");}
+                        delete Memory.creeps[name];
+                        console.log('Clearing non-existing creep memory:', name);
                     }
-                    try {creep.suicide();} catch(err) {console.log(err+": main.js creep.suicide()");}
+                }
+                if(!Game.creeps[name]) {
                     delete Memory.creeps[name];
                     console.log('Clearing non-existing creep memory:', name);
                 }
             }
-            if(!Game.creeps[name]) {
-                delete Memory.creeps[name];
-                console.log('Clearing non-existing creep memory:', name);
-            }
         }
-        for(var name in Game.rooms) {
-            if(!Game.rooms[name]) {
+        for(name in Game.rooms) {
+            if(Game.rooms[name]) {
+                var room = Game.rooms[name];
+                try {room.cycle();} catch(err) {console.log(err+": main.js room.cycle()");}
+            } else {
                 delete Memory.rooms[name];
                 console.log('Clearing non-existing room memory:', name);
             }
         }
-        for(var name in Game.rooms) {
-            var room = Game.rooms[name];
-            try {room.cycle();} catch(err) {console.log(err+": main.js room.cycle()");}
-        }
     });
-}
+};
