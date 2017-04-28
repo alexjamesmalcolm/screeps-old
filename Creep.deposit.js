7da96e2e3ad1e62c7273bdc88e4c67bc634ca3ec
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-28 01:41:04 -0400

    Update Creep.deposit.js

--- Creep.deposit.js
+++ Creep.deposit.js
@@ -1,15 +1,21 @@
 var CreepDeposit = function(input) {
     //console.log(JSON.stringify(input));
-    var creepDepositing = input.creepDepositing;
-    var structures = input.structures;
-    var amount = this.carry.energy;
-    var target;
-    var check = function(structure, structures, structureType) {
-        return structure.structureType == structureType
-        && structures.indexOf(structureType) != -1;
-    }
+    var creepDepositing, structures, amount, target, closestStructure, check, closestCreepDistance, closestStructureDistance, closestCreep;
+    creepDepositing = input.creepDepositing;
+    structures = input.structures;
+    amount = this.carry.energy;
+    check = function(structure, structures, structureType) {
+        if(structure.structureType == structureType) {
+            if(structures.indexOf(structureType) != -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    };
     if(structures.length > 0) {
-        var closestStructure;
         for(var i = 0; i < structures.length; i++) {
             var structureType = structures[i];
             if(closestStructure) {
@@ -21,16 +27,12 @@ var CreepDeposit = function(input) {
                             switch(structureType) {
                                 case STRUCTURE_EXTENSION:
                                     return structure.energyCapacity - structure.energy > 0;
-                                    break;
                                 case STRUCTURE_SPAWN:
                                     return structure.energyCapacity - structure.energy > 0;
-                                    break;
                                 case STRUCTURE_STORAGE:
                                     return structure.storeCapacity - _.sum(structure.store) > 0;
-                                    break;
                                 case STRUCTURE_CONTAINER:
                                     return structure.storeCapacity - _.sum(structure.store) > 0;
-                                    break;
                                 case STRUCTURE_TOWER:
                                     return structure.energyCapacity - structure.energy > 0;
                             }
@@ -40,10 +42,10 @@ var CreepDeposit = function(input) {
             }
         }
         //console.log(closestStructure);
-        var closestStructureDistance = this.pos.getRangeTo(closestStructure);
+        closestStructureDistance = this.pos.getRangeTo(closestStructure);
     }
     if(creepDepositing) {
-        var closestCreep = this.pos.findClosestByRange(FIND_MY_CREEPS, {
+        closestCreep = this.pos.findClosestByRange(FIND_MY_CREEPS, {
             filter: function(creep) {
                 if(creep.memory.needEnergy) {
                     if(creep.carryCapacity - creep.carry.energy >= amount) {
@@ -53,7 +55,7 @@ var CreepDeposit = function(input) {
             }
         });
         //console.log(closestCreep);
-        var closestCreepDistance = this.pos.getRangeTo(closestCreep);
+        closestCreepDistance = this.pos.getRangeTo(closestCreep);
     }
     if(closestCreepDistance <= closestStructureDistance) {
         target = closestCreep;
