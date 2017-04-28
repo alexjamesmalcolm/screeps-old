03e316339cf4c76c533971297aedb2c0fe374b8d
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-28 18:35:57 -0400

    Update Room.builderSpawn.js

--- Room.builderSpawn.js
+++ Room.builderSpawn.js
@@ -1,5 +1,5 @@
 var optimalBuilder = function(room) {
-    var carryBodyparts, workBodyparts, moveBodyparts, creepCost, bodyparts, progressPerTick, i;
+    var carryBodyparts, workBodyparts, moveBodyparts, creepCost, bodyparts, progressPerTick;
     carryBodyparts = 1;
     //x = 1
     workBodyparts = Math.floor((2 * room.energyAvailable - (2 * BODYPART_COST[CARRY] + BODYPART_COST[MOVE]) * carryBodyparts) / (BODYPART_COST[MOVE] + 2 * BODYPART_COST[WORK]));
@@ -10,13 +10,13 @@ var optimalBuilder = function(room) {
     creepCost = carryBodyparts * BODYPART_COST[CARRY] + workBodyparts * BODYPART_COST[WORK] + moveBodyparts * BODYPART_COST[MOVE];
     //t = x * c + y * w + z * m
     bodyparts = [];
-    for(i = 0; i < moveBodyparts; i++) {
+    for(var i = 0; i < moveBodyparts; i++) {
         bodyparts.push(MOVE);
     }
-    for(i = 0; i < workBodyparts; i++) {
+    for(var i = 0; i < workBodyparts; i++) {
         bodyparts.push(WORK);
     }
-    for(i = 0; i < carryBodyparts; i++) {
+    for(var i = 0; i < carryBodyparts; i++) {
         bodyparts.push(CARRY);
     }
     if(workBodyparts > 0 && moveBodyparts > 0 && carryBodyparts > 0) {
@@ -98,26 +98,19 @@ var RoomBuilderSpawn = function() {
     if(builders.length > 0) {
         if(builder) {
             if(builder.workBodyparts > builders[0].getActiveBodyparts(WORK)) {
-                if(this.memory.spawns > 0) {
-                    builders[0].memory.recycle = true;
-                    this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
-                    this.memory.spawns[0].memory.spawning = Game.time;
-                }
+                builders[0].memory.recycle = true;
+                this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                this.memory.spawns[0].memory.spawning = Game.time;
             } else if(timeToFinish > 720) {
-                if(this.memory.spawns > 0) { 
-                    if(this.memory.harvestPerTick < buildPerTick) {
-                        this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
-                        this.memory.spawns[0].memory.spawning = Game.time;
-                    }
-                }
+                console.log(builder.bodyparts);
+                this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                this.memory.spawns[0].memory.spawning = Game.time;
             }
         }
     } else {
         if(builder) {
-            if(this.memory.spawns > 0) {
-                this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
-                this.memory.spawns[0].memory.spawning = Game.time;
-            }
+            this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+            this.memory.spawns[0].memory.spawning = Game.time;
         }
     }
     this.memory.buildPerTick = buildPerTick;
