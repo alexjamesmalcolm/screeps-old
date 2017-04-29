122e42c072372bc7731a270c990f10bdf7c2a5c2
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-29 01:41:28 -0400

    Update Room.courierSpawn.js

--- Room.courierSpawn.js
+++ Room.courierSpawn.js
@@ -68,12 +68,12 @@ var RoomCourierSpawn = function() {
                         this.memory.spawns[0].memory.spawning = Game.time;
                     }
                 }
-                if(transitEnergy * 0.33 > totalCarry + CARRY_CAPACITY * courier.carryBodyparts) {
+                if(transitEnergy * 0.5 > totalCarry + CARRY_CAPACITY * courier.carryBodyparts) {
                     this.memory.spawns[0].createCreep(courier.bodyparts, undefined, {role: 'courier'});
                     this.memory.spawns[0].memory.spawning = Game.time;
                 }
             }
-            if(transitEnergy * 0.33 < totalCarry) {
+            if(transitEnergy * 0.5 < totalCarry) {
                 if(couriers.length > 2) {
                     couriers[0].memory.recycle = true;
                 }
