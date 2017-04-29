cdefc486507021e2518c21fdf0e49ab507deeb5b
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-04-29 02:28:04 -0400

    Update Creep.roleSpawnKeeper.js

--- Creep.roleSpawnKeeper.js
+++ Creep.roleSpawnKeeper.js
@@ -1,5 +1,27 @@
 var CreepRoleSpawnKeeper = function() {
-    console.log();
+    if(this.memory.collecting) {
+        if(this.carry.energy === this.carryCapacity) {
+            this.memory.collecting = false;
+            this.say('Delivering');
+        }
+    } else {
+        if(this.carry.energy === 0) {
+            this.memory.collecting = true;
+            this.say('Collecting');
+        }
+    }
+    if(this.memory.collecting) {
+        this.collect({
+            resource: RESOURCE_ENERGY,
+            amount: this.carryCapacity,
+            structures: [STRUCTURE_STORAGE, STRUCTURE_CONTAINER]
+        });
+    } else {
+        this.deposit({
+            creepDepositing: false,
+            structures: [STRUCTURE_SPAWN, STRUCTURE_EXTENSION, STRUCTURE_TOWER]
+        });
+    }
 };
 
 module.exports = CreepRoleSpawnKeeper;
