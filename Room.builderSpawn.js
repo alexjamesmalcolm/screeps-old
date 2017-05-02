e40185acefd0038a3e0cc0c5639df85c2b63b396
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-02 18:12:41 -0400

    Update Room.builderSpawn.js

--- Room.builderSpawn.js
+++ Room.builderSpawn.js
@@ -96,23 +96,27 @@ var RoomBuilderSpawn = function() {
         }
     }
     if(builders.length > 0) {
-        if(builder) {
-            if(this.memory.energyPercent === 1) {
-                if(builder.workBodyparts > builders[0].getActiveBodyparts(WORK)) {
-                    builders[0].memory.recycle = true;
-                    this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
-                    this.memory.spawns[0].memory.busy = Game.time;
-                } else if(timeToFinish > 720) {
-                    console.log(builder.bodyparts);
-                    this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
-                    this.memory.spawns[0].memory.busy = Game.time;
+        if(this.memory.spawns.length > 0) {
+            if(builder) {
+                if(this.memory.energyPercent === 1) {
+                    if(builder.workBodyparts > builders[0].getActiveBodyparts(WORK)) {
+                        builders[0].memory.recycle = true;
+                        this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                        this.memory.spawns[0].memory.busy = Game.time;
+                    } else if(timeToFinish > 720) {
+                        console.log(builder.bodyparts);
+                        this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                        this.memory.spawns[0].memory.busy = Game.time;
+                    }
                 }
             }
         }
     } else {
-        if(builder) {
-            this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
-            this.memory.spawns[0].memory.busy = Game.time;
+        if(this.memory.spawns.length > 0) {
+            if(builder) {
+                this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                this.memory.spawns[0].memory.busy = Game.time;
+            }
         }
     }
     this.memory.buildPerTick = buildPerTick;
