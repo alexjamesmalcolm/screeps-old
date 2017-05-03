8259351e4d456b95ef5808bd85d15a518ea8e45e
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-03 01:34:11 -0400

    Update Creep.roleSpawnKeeper.js

--- Creep.roleSpawnKeeper.js
+++ Creep.roleSpawnKeeper.js
@@ -1,4 +1,5 @@
 var CreepRoleSpawnKeeper = function() {
+    console.log("this: "+this);
     if(this.memory.collecting) {
         if(this.carry.energy === this.carryCapacity) {
             this.memory.collecting = false;
@@ -11,6 +12,8 @@ var CreepRoleSpawnKeeper = function() {
         }
     }
     if(this.memory.collecting) {
+        console.log("Collecting");
+        var target;
         var storage = this.room.find(FIND_STRUCTURES, {
             filter: function(structure) {
                 if(structure.structureType === STRUCTURE_STORAGE) {
@@ -20,18 +23,26 @@ var CreepRoleSpawnKeeper = function() {
                 }
             }
         });
+        console.log("storage: "+storage);
         var droppedEnergy = this.room.find(FIND_DROPPED_ENERGY);
         if(droppedEnergy.length > 0 && droppedEnergy[0].amount > 50) {
+            console.log("If");
+            target = droppedEnergy[0];
+            this.room.visual.circle(target.pos, {fill: 'transparent', radius: 0.55, stroke: 'red'});
             if(this.pickup(droppedEnergy[0]) === ERR_NOT_IN_RANGE) {
-                this.moveTo(droppedEnergy[0]);
-                this.room.visual.circle(droppedEnergy[0].pos, {fill: 'transparent', radius: 0.55, stroke: 'red'});
+                this.moveTo(target);
             }
         } else {
-            if(this.transfer(storage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                this.moveTo(storage);
-                this.room.visual.circle(storage.pos, {fill: 'transparent', radius: 0.55, stroke: 'red'});
+            console.log("Else");
+            target = storage;
+            console.log(target.pos);
+            this.room.visual.circle(target.pos, {fill: 'transparent', radius: 0.55, stroke: 'red'});
+            console.log(this.withdraw(target, RESOURCE_ENERGY));
+            if(this.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                this.moveTo(target);
             }
         }
+        console.log("target: "+target);
     } else {
         this.deposit({
             creepDepositing: false,
