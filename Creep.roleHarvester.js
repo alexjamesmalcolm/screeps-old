c7848d0fc7201eb8bd30875167467cc62ac6e894
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-04 16:36:31 -0400

    Update Creep.roleHarvester.js

--- Creep.roleHarvester.js
+++ Creep.roleHarvester.js
@@ -17,13 +17,26 @@ var roleHarvester = function() {
             this.moveTo(source);
         }
     } else {
-        try {this.deposit({
-            creepDepositing: false,
-            structures: [STRUCTURE_CONTAINER, STRUCTURE_STORAGE, STRUCTURE_EXTENSION, STRUCTURE_SPAWN]
-        });} catch(err) {console.log(err+": Creep.roleHarvester.js this.deposit()");}
+        var link = this.pos.findClosestByRange(FIND_STRUCTURES, {filter: function(structure) {
+            if(structure.structureType === STRUCTURE_LINK) {
+                return true;
+            } else {
+                return false;
+            }
+        }});
+        if(this.pos.inRangeTo(link, 2)) {
+            var result = this.transfer(link, RESOURCE_ENERGY);
+            if(result === ERR_NOT_IN_RANGE) {
+                this.moveTo(link);
+            }
+        } else {
+            try {this.deposit({
+                creepDepositing: false,
+                structures: [STRUCTURE_CONTAINER, STRUCTURE_STORAGE, STRUCTURE_EXTENSION, STRUCTURE_SPAWN]
+            });} catch(err) {console.log(err+": Creep.roleHarvester.js this.deposit()");}
+        }
         try {this.passiveRepair();} catch(err) {console.log(err+": Creep.roleHarvester.js this.passiveRepair()");}
     }
-    //this.memory.lastPos = this.pos;
 };
 
 module.exports = roleHarvester;
