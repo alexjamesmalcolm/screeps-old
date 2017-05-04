490a5ee6bd55fe8921ab0b9046eb823b5a647751
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-04 17:34:05 -0400

    Update Room.findAll.js

--- Room.findAll.js
+++ Room.findAll.js
@@ -2,6 +2,15 @@ var RoomFindAll = function() {
     var found = {};
     found.sources = this.find(FIND_SOURCES);
     found.myCreeps = this.find(FIND_MY_CREEPS);
+    found.links = this.find(FIND_STRUCTURES, {
+        filter: function(structure) {
+            if(structure.structure === STRUCTURE_LINK) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
     this.memory.found = found;
 };
 
