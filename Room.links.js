95bc0bf11580c2a85e0e11fd2461b3f538e54ef2
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-04 16:51:14 -0400

    Update Room.links.js

--- Room.links.js
+++ Room.links.js
@@ -1,4 +1,28 @@
 var RoomLink = function() {
+    var linkClosestToStorage = this.storage.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: function(structure) {
+            if(structure.structureType === STRUCTURE_LINK) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+    var links = this.find(FIND_STRUCTURES, {
+        filter: function(structure) {
+            if(structure.structureType === STRUCTURE_LINK) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+    for(var i = 0; i < links.length; i++) {
+        var link = links[i];
+        if(link.id !== linkClosestToStorage.id) {
+            link.transferEnergy(linkClosestToStorage);
+        }
+    }
 };
 
 module.exports = RoomLink;
