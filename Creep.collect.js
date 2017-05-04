a216a77152c4b176230dec69dc0c08bca1080708
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-04 18:10:46 -0400

    Update Creep.collect.js

--- Creep.collect.js
+++ Creep.collect.js
@@ -12,24 +12,24 @@ var CreepCollect = function(input) {
     var resource = input.resource;
     //var amount = this.carryCapacity - _.sum(this.carry);
     var amount = 50;
-    var structures = input.structures;
-    var closestStructure = this.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: function(structure) {
-            if(structure.structureType == STRUCTURE_CONTAINER && structures.indexOf(STRUCTURE_CONTAINER) != -1) {
-                return _.sum(structure.store) >= amount;
-            } else if(structure.structureType == STRUCTURE_STORAGE && structures.indexOf(STRUCTURE_STORAGE) != -1) {
-                return _.sum(structure.store) >= amount;
-            } else if(structure.structureType == STRUCTURE_EXTENSION && structures.indexOf(STRUCTURE_EXTENSION) != -1) {
-                return structure.energy >= amount;
-            } else if(structure.structureType == STRUCTURE_SPAWN && structures.indexOf(STRUCTURE_SPAWN) != -1) {
-                return structure.energy >= amount;
-            } else if(structure.structureType == STRUCTURE_TOWER && structures.indexOf(STRUCTURE_TOWER) != -1) {
-                return structure.energy >= amount;
-            } else if(structure.structureType == STRUCTURE_LINK && structures.indexOf(STRUCTURE_LINK) != -1) {
-                return structures.energy >= amount;
-            }
+    var acceptableStructures = input.structures;
+    var structures = this.room.memory.found.structures;
+    structures = _.filter(structures, function(structure) {
+        if(structure.structureType == STRUCTURE_CONTAINER && acceptableStructures.indexOf(STRUCTURE_CONTAINER) != -1) {
+            return _.sum(structure.store) >= amount;
+        } else if(structure.structureType == STRUCTURE_STORAGE && acceptableStructures.indexOf(STRUCTURE_STORAGE) != -1) {
+            return _.sum(structure.store) >= amount;
+        } else if(structure.structureType == STRUCTURE_EXTENSION && acceptableStructures.indexOf(STRUCTURE_EXTENSION) != -1) {
+            return structure.energy >= amount;
+        } else if(structure.structureType == STRUCTURE_SPAWN && acceptableStructures.indexOf(STRUCTURE_SPAWN) != -1) {
+            return structure.energy >= amount;
+        } else if(structure.structureType == STRUCTURE_TOWER && acceptableStructures.indexOf(STRUCTURE_TOWER) != -1) {
+            return structure.energy >= amount;
+        } else if(structure.structureType == STRUCTURE_LINK && acceptableStructures.indexOf(STRUCTURE_LINK) != -1) {
+            return structure.energy >= amount;
         }
     });
+    var closestStructure = this.pos.findClosestByPath(structures);
     var closestStructureDistance = this.pos.getRangeTo(closestStructure);
     var droppedEnergy = this.pos.findClosestByRange(FIND_DROPPED_ENERGY);
     var droppedEnergyDistance;
