c5c26745614f5e6838f7e618ab6442487f7da333
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-04 18:01:52 -0400

    Update Room.findAll.js

--- Room.findAll.js
+++ Room.findAll.js
@@ -2,6 +2,7 @@ var RoomFindAll = function() {
     var found = {};
     found.sources = this.find(FIND_SOURCES);
     found.myCreeps = this.find(FIND_MY_CREEPS);
+    found.mySpawns = this.find(FIND_MY_SPAWNS);
     found.structures = this.find(FIND_STRUCTURES);
     found.links = _.filter(found.structures, {'structureType': STRUCTURE_LINK});
     found.towers = _.filter(found.structures, {'structureType': STRUCTURE_TOWER});
