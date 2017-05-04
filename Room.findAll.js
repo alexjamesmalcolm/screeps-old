603b02554c957282047761ab83f1d2915e45daf8
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-04 17:31:15 -0400

    Update Room.findAll.js

--- Room.findAll.js
+++ Room.findAll.js
@@ -1,5 +1,8 @@
 var RoomFindAll = function() {
-    
+    var found = {};
+    found.sources = this.find(FIND_SOURCES);
+    found.myCreeps = this.find(FIND_MY_CREEPS);
+    this.memory.found = found;
 };
 
 module.exports = RoomFindAll;
