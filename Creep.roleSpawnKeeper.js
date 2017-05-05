1a436bdd051747707239a5e892b434fb1136f568
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-05 06:46:02 -0400

    Update Creep.roleSpawnKeeper.js

--- Creep.roleSpawnKeeper.js
+++ Creep.roleSpawnKeeper.js
@@ -42,7 +42,7 @@ var CreepRoleSpawnKeeper = function() {
                 if(this.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                     this.moveTo(target);
                 }
-            } else if(storage.store[RESOURCE_ENERGY] > 1000) {
+            } else if(storage.store[RESOURCE_ENERGY] > 1000 + this.carryCapacity) {
                 target = storage;
                 this.room.visual.circle(target.pos, {fill: 'transparent', radius: 0.55, stroke: 'red'});
                 if(this.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
