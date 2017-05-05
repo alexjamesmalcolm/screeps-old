4503cf94506964862c2113a61bd08903447c0980
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-05 02:54:29 -0400

    Update Room.links.js

--- Room.links.js
+++ Room.links.js
@@ -1,19 +1,21 @@
 var RoomLink = function() {
-    var linkClosestToStorage = this.storage.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: function(structure) {
-            if(structure.structureType === STRUCTURE_LINK) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    });
     var links = this.memory.found.links;
-    for(var i = 0; i < links.length; i++) {
-        var link = links[i];
-        if(link.id !== linkClosestToStorage.id) {
-            if(link.energy <= linkClosestToStorage.energyCapacity - linkClosestToStorage.energy) {
-                link.transferEnergy(linkClosestToStorage);
+    if(links.length >= 2) {
+        var linkClosestToStorage = this.storage.pos.findClosestByRange(links, {
+            filter: function(structure) {
+                if(structure.structureType === STRUCTURE_LINK) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        for(var i = 0; i < links.length; i++) {
+            var link = links[i];
+            if(link.id !== linkClosestToStorage.id) {
+                if(link.energy <= linkClosestToStorage.energyCapacity - linkClosestToStorage.energy) {
+                    link.transferEnergy(linkClosestToStorage);
+                }
             }
         }
     }
