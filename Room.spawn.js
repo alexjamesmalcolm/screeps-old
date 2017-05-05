4c2d8c391feeef89486acb81a9bb92ada2d1b97e
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-05 03:19:25 -0400

    Update Room.spawn.js

--- Room.spawn.js
+++ Room.spawn.js
@@ -15,16 +15,23 @@ var checkSpawns = function(room) {
 };
 
 var RoomSpawns = function() {
+    var harvesters = _.filter(this.memory.found.myCreeps, function(creep) {
+        if(creep.memory.role === 'harvester') {
+            return true;
+        }
+    });
+    if(harvesters.length > 0) {
+        checkSpawns(room);
+        try {this.spawnKeeperSpawn();} catch(err) {console.log(err+": Room.cycle.js this.spawnKeeperSpawn()");}
+        checkSpawns(room);
+        try {this.courierSpawn();} catch(err) {console.log(err+": Room.cycle.js this.courierSpawn()");}
+        checkSpawns(room);
+        try {this.upgraderSpawn();} catch(err) {console.log(err+": Room.cycle.js this.upgraderSpawn()");}
+        checkSpawns(room);
+        try {this.builderSpawn();} catch(err) {console.log(err+": Room.cycle.js this.builderSpawn()");}
+    }
     checkSpawns(room);
     try {this.harvesterSpawn();} catch(err) {console.log(err+": Room.cycle.js this.harvesterSpawn()");}
-    checkSpawns(room);
-    try {this.spawnKeeperSpawn();} catch(err) {console.log(err+": Room.cycle.js this.spawnKeeperSpawn()");}
-    checkSpawns(room);
-    try {this.courierSpawn();} catch(err) {console.log(err+": Room.cycle.js this.courierSpawn()");}
-    checkSpawns(room);
-    try {this.upgraderSpawn();} catch(err) {console.log(err+": Room.cycle.js this.upgraderSpawn()");}
-    checkSpawns(room);
-    try {this.builderSpawn();} catch(err) {console.log(err+": Room.cycle.js this.builderSpawn()");}
 };
 
 module.exports = RoomSpawns;
