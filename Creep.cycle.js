c2ddc62b05a41862ec2bedcd30ba7114b52df99a
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-06 16:49:31 -0400

    Update Creep.cycle.js

--- Creep.cycle.js
+++ Creep.cycle.js
@@ -17,21 +17,23 @@ var CreepCycle = function() {
         }*/
     }
     if(this.memory.sick) {
-        try {this.sick();} catch(err) {console.log(err+": Creep.cycle.js this.sick()");}
+        try {this.sick();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.sick()");}
     } else if(this.memory.recycle) {
-        try {stateRecycle.run(this);} catch(err) {console.log(err+": Creep.cycle.js stateRecycle.run(this)");}
+        try {stateRecycle.run(this);} catch(err) {console.log(err+" "+this+": Creep.cycle.js stateRecycle.run(this)");}
     } else if(this.memory.role == 'harvester') {
-        try {this.roleHarvester();} catch(err) {console.log(err+": Creep.cycle.js this.roleHarvester()");}
+        try {this.roleHarvester();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleHarvester()");}
     } else if(this.memory.role == 'builder') {
-        try {this.roleBuilder();} catch(err) {console.log(err+": Creep.cycle.js this.roleBuilder()");}
+        try {this.roleBuilder();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleBuilder()");}
     } else if(this.memory.role == 'upgrader') {
-        try {this.roleUpgrader();} catch(err) {console.log(err+": Creep.cycle.js this.roleUpgrader()");}
+        try {this.roleUpgrader();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleUpgrader()");}
     } else if(this.memory.role == 'fighter') {
-        try {this.roleFighter();} catch(err) {console.log(err+": Creep.cycle.js this.roleFighter()");}
+        try {this.roleFighter();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleFighter()");}
     } else if(this.memory.role == 'courier') {
-        try {this.roleCourier();} catch(err) {console.log(err+": Creep.cycle.js this.roleCourier()");}
+        try {this.roleCourier();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleCourier()");}
     } else if(this.memory.role == 'spawnKeeper') {
-        try {this.roleSpawnKeeper();} catch(err) {console.log(err+ ": Creep.cycle.js this.roleSpawnKeeper()");}
+        try {this.roleSpawnKeeper();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleSpawnKeeper()");}
+    } else if(this.memory.role == 'claimer') {
+        try {this.roleClaimer();} catch(err) {console.log(err+" "+this+": Creep.cycle.js this.roleClaimer()");}
     }
 };
 
