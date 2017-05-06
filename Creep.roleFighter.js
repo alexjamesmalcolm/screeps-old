a1155dbc310e9952070116b19fd0bdd1e7e0cd41
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-06 17:17:36 -0400

    Update Creep.roleFighter.js

--- Creep.roleFighter.js
+++ Creep.roleFighter.js
@@ -1,5 +1,10 @@
 var roleFighter = function() {
     var enemy = this.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+    var hostileFlags = _.filter(Game.flags, function(flag) {
+        if(flag.color === COLOR_RED) {
+            return true;
+        }
+    });
     if (enemy) {
         console.log(enemy);
         console.log(this.attack(enemy));
@@ -7,6 +12,9 @@ var roleFighter = function() {
         if (this.attack(enemy) == ERR_NOT_IN_RANGE) {
             this.moveTo(enemy);
         }
+    } else if(hostileFlags.length > 0) {
+        var nearestFlag = this.pos.findClosestByPath(hostileFlags);
+        this.moveTo(nearestFlag);
     } else {
         this.moveTo(new RoomPosition(25, 20, 'W94S6'));
         //creep.move(Math.floor(Math.random()*8+1));
