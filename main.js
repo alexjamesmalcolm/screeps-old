60598ce6a741e914118aa9bd4c8af633d10ddcf9
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-06 17:05:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 try {
-    Creep.prototype.roleSpawnKeeper = require('Creep.roleSpawnKeeper');
     Creep.prototype.collect = require('Creep.collect');
     Creep.prototype.cycle = require('Creep.cycle');
     Creep.prototype.deposit = require('Creep.deposit');
@@ -7,26 +6,27 @@ try {
     Creep.prototype.passiveRepair = require('Creep.passiveRepair');
     Creep.prototype.pathing = require('Creep.pathing');
     Creep.prototype.roleBuilder = require('Creep.roleBuilder');
+    Creep.prototype.roleClaimer = require('Creep.roleClaimer');
     Creep.prototype.roleCourier = require('Creep.roleCourier');
     Creep.prototype.roleFighter = require('Creep.roleFighter');
     Creep.prototype.roleHarvester = require('Creep.roleHarvester');
+    Creep.prototype.roleSpawnKeeper = require('Creep.roleSpawnKeeper');
     Creep.prototype.roleUpgrader = require('Creep.roleUpgrader');
-    Creep.prototype.roleClaimer = require('Creep.roleClaimer');
     Creep.prototype.sick = require('Creep.sick');
     Creep.prototype.weight = require('Creep.weight');
     Room.prototype.builderSpawn = require('Room.builderSpawn');
     Room.prototype.courierSpawn = require('Room.courierSpawn');
     Room.prototype.cycle = require('Room.cycle');
+    Room.prototype.fighterSpawn = require('Room.fighterSpawn');
+    Room.prototype.findAll = require('Room.findAll');
     Room.prototype.harvesterSpawn = require('Room.harvesterSpawn');
+    Room.prototype.links = require('Room.links');
     Room.prototype.paths = require('Room.paths');
     Room.prototype.roadBuilder = require('Room.roadBuilder');
+    Room.prototype.spawn = require('Room.spawn');
+    Room.prototype.spawnKeeperSpawn = require('Room.spawnKeeperSpawn');
     Room.prototype.towers = require('Room.towers');
     Room.prototype.upgraderSpawn = require('Room.upgraderSpawn');
-    Room.prototype.spawnKeeperSpawn = require('Room.spawnKeeperSpawn');
-    Room.prototype.fighterSpawn = require('Room.fighterSpawn');
-    Room.prototype.links = require('Room.links');
-    Room.prototype.spawn = require('Room.spawn');
-    Room.prototype.findAll = require('Room.findAll');
     Spawn.prototype.cycle = require('Spawn.cycle');
 } catch(err) {console.log(err+": main.js 1");}
 const profiler = require('screeps-profiler');
