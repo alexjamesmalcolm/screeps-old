2449fbaab02a25a693ac35ed3ccf7a171c3bbf6c
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-06 18:18:35 -0400

    Update Room.builderSpawn.js

--- Room.builderSpawn.js
+++ Room.builderSpawn.js
@@ -1,65 +1,126 @@
-var roleBuilder = function() {
-    if(this.memory.building && this.carry.energy === 0) {
-        this.memory.building = false;
-        this.memory.needEnergy = true;
-        this.say('Need Energy');
+var optimalBuilder = function(room) {
+    var carryBodyparts, workBodyparts, moveBodyparts, creepCost, bodyparts, progressPerTick;
+    carryBodyparts = 2;
+    //x = 1
+    workBodyparts = Math.floor((2 * room.energyAvailable - (2 * BODYPART_COST[CARRY] + BODYPART_COST[MOVE]) * carryBodyparts) / (BODYPART_COST[MOVE] + 2 * BODYPART_COST[WORK]));
+    //y = (2 * t - (2 * c + m) * x) / (m + 2 * w);
+    moveBodyparts = Math.floor((carryBodyparts + workBodyparts) / 2);
+    //z = (x + y) / 2
+    //W = x + y
+    creepCost = carryBodyparts * BODYPART_COST[CARRY] + workBodyparts * BODYPART_COST[WORK] + moveBodyparts * BODYPART_COST[MOVE];
+    //t = x * c + y * w + z * m
+    bodyparts = [];
+    for(var i = 0; i < moveBodyparts; i++) {
+        bodyparts.push(MOVE);
     }
-    if(!this.memory.building && this.carry.energy === this.carryCapacity) {
-        this.memory.building = true;
-        this.memory.needEnergy = false;
-        this.say('Building');
+    for(var i = 0; i < workBodyparts; i++) {
+        bodyparts.push(WORK);
     }
-    if(this.memory.building) {
-        var flags = Game.flags;
-        var myConstructionSites = this.room.memory.found.myConstructionSites;
-        var wipSpawns = _.filter(myConstructionSites, function(site) {
-            if(site.structureType === STRUCTURE_SPAWN) {
-                return true;
-            }
-        });
-        var orangeFlags = _.filter(flags, function(flag) {
-            if(flag.color === COLOR_ORANGE) {
-                return true;
-            }
-        });
-        if(wipSpawns.length > 0) {
-            var spawn = wipSpawns[0];
-            var result = this.build(spawn);
-            if(result === ERR_NOT_IN_RANGE) {
-                this.moveTo(spawn);
-            }
-        } else if(orangeFlags.length > 0) {
-            this.moveTo(orangeFlags[0]);
-        } else {
-            var result = this.maintainBuild({
-                activeRepair: true,
-                structures: [
-                    STRUCTURE_CONTAINER,
-                    STRUCTURE_STORAGE,
-                    STRUCTURE_ROAD
-                ]
-            });
-            if(result == ERR_NOT_FOUND) {
-                //this.memory.recycle = true;
-            }
-            this.passiveRepair();
-        }
-    } else {
-        if(this.room.storage) {
-            this.collect({
-                resource: RESOURCE_ENERGY,
-                amount: this.carryCapacity,
-                structures:[STRUCTURE_STORAGE]
-            });
-        } else {
-            var sources = this.room.memory.found.sources;
-            var source = this.pos.findClosestByPath(sources);
-            var result = this.harvest(source);
-            if(result === ERR_NOT_IN_RANGE) {
-                this.moveTo(source);
-            }
-        }
+    for(var i = 0; i < carryBodyparts; i++) {
+        bodyparts.push(CARRY);
+    }
+    if(workBodyparts > 0 && moveBodyparts > 0 && carryBodyparts > 0) {
+        return {
+            carryBodyparts: carryBodyparts,
+            workBodyparts: workBodyparts,
+            moveBodyparts: moveBodyparts,
+            creepCost: creepCost,
+            bodyparts: bodyparts,
+            progressPerTick: progressPerTick
+        };
     }
 };
+var RoomBuilderSpawn = function() {
+    var constructionProjects = this.find(FIND_CONSTRUCTION_SITES);
+    var progressTotal = 0;
+    var progress = 0;
+    constructionProjects.forEach(function(constructionProject) {
+        progressTotal = progressTotal + constructionProject.progressTotal;
+        progress = progress + constructionProject.progress;
+    });
+    var remainingProgress = progressTotal - progress;
+    
+    var structures = this.find(FIND_STRUCTURES);
+    var hitsTotal = 0;
+    var hits = 0;
+    structures.forEach(function(structure) {
+        if(structure.hitsMax > 0 && structure.hits) {
+            hitsTotal = hitsTotal + structure.hitsMax;
+            hits = hits + structure.hits;
+        }
+    });
+    var repairNeeded = hitsTotal - hits;
+    //console.log("hitsTotal: "+hitsTotal);
+    //console.log("hits: "+hits);
+    //REPAIR_POWER
+    //console.log("remainingProgress: "+remainingProgress);
+    var builders = this.find(FIND_MY_CREEPS, {
+        filter: function(creep) {
+            if(creep.memory.recycle) {
+                return false;
+            } else if(creep.memory.role == 'builder') {
+                return true;
+            }
+        }
+    });
+    builders.sort(function(a, b) {
+        var a_work = a.getActiveBodyparts(WORK);
+        var b_work = b.getActiveBodyparts(WORK);
+        return a_work - b_work;
+    });
+    var builder = optimalBuilder(this);
+    var buildPerTick = 0;
+    var repairPerTick = 0;
+    builders.forEach(function(creep) {
+        buildPerTick = buildPerTick + BUILD_POWER * creep.getActiveBodyparts(WORK);
+        repairPerTick = repairPerTick + REPAIR_POWER * creep.getActiveBodyparts(WORK);
+    });
+    var timeToFinish;
+    var timeToBuild;
+    if(buildPerTick > 0) {
+        timeToBuild = remainingProgress / buildPerTick;
+    }
+    var timeToRepair;
+    if(repairPerTick > 0) {
+        timeToRepair = repairNeeded / repairPerTick;
+    }
+    if(buildPerTick > 0) {
+        if(repairPerTick > 0) {
+            timeToFinish = timeToBuild + timeToRepair;
+        } else {
+            timeToFinish = timeToBuild;
+        }
+    } else {
+        if(repairPerTick > 0) {
+            timeToFinish = timeToRepair;
+        }
+    }
+    if(builders.length > 0) {
+        if(this.memory.spawns.length > 0) {
+            if(builder) {
+                if(this.memory.energyPercent === 1) {
+                    if(builder.workBodyparts > builders[0].getActiveBodyparts(WORK)) {
+                        builders[0].memory.recycle = true;
+                        this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                        this.memory.spawns[0].memory.busy = Game.time;
+                    } else if(timeToFinish > 720) {
+                        console.log(builder.bodyparts);
+                        this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                        this.memory.spawns[0].memory.busy = Game.time;
+                    }
+                }
+            }
+        }
+    } else {
+        if(this.memory.spawns.length > 0) {
+            if(builder) {
+                this.memory.spawns[0].createCreep(builder.bodyparts, undefined, {role: 'builder'});
+                this.memory.spawns[0].memory.busy = Game.time;
+            }
+        }
+    }
+    this.memory.buildPerTick = buildPerTick;
+    this.memory.remainingProgress = remainingProgress;
+};
 
-module.exports = roleBuilder;
+module.exports = RoomBuilderSpawn;
