a57ede22980770ca4693070c00bb67067321ffe6
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-06 17:49:18 -0400

    Update Creep.roleBuilder.js

--- Creep.roleBuilder.js
+++ Creep.roleBuilder.js
@@ -10,24 +10,55 @@ var roleBuilder = function() {
         this.say('Building');
     }
     if(this.memory.building) {
-        var result = this.maintainBuild({
-            activeRepair: true,
-            structures: [
-                STRUCTURE_CONTAINER,
-                STRUCTURE_STORAGE,
-                STRUCTURE_ROAD
-            ]
+        var flags = this.room.memory.found.flags;
+        var myConstructionSites = this.room.memory.found.myConstructionSites;
+        var wipSpawns = _.filter(myConstructionSites, function(site) {
+            if(site.structureType === STRUCTURE_SPAWN) {
+                return true;
+            }
         });
-        if(result == ERR_NOT_FOUND) {
-            this.memory.recycle = true;
+        var orangeFlags = _.filter(flags, function(flag) {
+            if(flag.color === COLOR_ORANGE) {
+                return true;
+            }
+        });
+        if(wipSpawns.length > 0) {
+            var spawn = wipSpawns[0];
+            var result = this.build(spawn);
+            if(result === ERR_NOT_IN_RANGE) {
+                this.moveTo(spawn);
+            }
+        } else if(orangeFlags.length > 0) {
+            this.moveTo(orangeFlags[0]);
+        } else {
+            var result = this.maintainBuild({
+                activeRepair: true,
+                structures: [
+                    STRUCTURE_CONTAINER,
+                    STRUCTURE_STORAGE,
+                    STRUCTURE_ROAD
+                ]
+            });
+            if(result == ERR_NOT_FOUND) {
+                this.memory.recycle = true;
+            }
+            this.passiveRepair();
         }
-        this.passiveRepair();
     } else {
-        this.collect({
-            resource: RESOURCE_ENERGY,
-            amount: this.carryCapacity,
-            structures:[STRUCTURE_STORAGE]
-        });
+        if(this.room.storage) {
+            this.collect({
+                resource: RESOURCE_ENERGY,
+                amount: this.carryCapacity,
+                structures:[STRUCTURE_STORAGE]
+            });
+        } else {
+            var sources = this.room.memory.found.sources;
+            var source = this.pos.findClosestByPath(sources);
+            var result = this.harvest(source);
+            if(result === ERR_NOT_IN_RANGE) {
+                this.moveTo(source);
+            }
+        }
     }
 };
 
