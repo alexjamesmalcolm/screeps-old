73adcdac7eb2e780fb9b099760666cff26298ca1
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-08 02:54:16 -0400

    Update Creep.roleHarvester.js

--- Creep.roleHarvester.js
+++ Creep.roleHarvester.js
@@ -1,3 +1,5 @@
+var sourceAvailability = function(source) {
+};
 var roleHarvester = function() {
     if(this.memory.harvesting) {
         if(this.carry.energy === this.carryCapacity) {
@@ -12,7 +14,8 @@ var roleHarvester = function() {
     }
 
     if(this.memory.harvesting) {
-        var source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE); //Inefficient
+        var activeSources = this.room.memory.found.activeSources;
+        var source = this.pos.findClosestByPath(activeSources); //Inefficient
         if(source) {
             if(this.harvest(source) == ERR_NOT_IN_RANGE) {
                 this.moveTo(source);
