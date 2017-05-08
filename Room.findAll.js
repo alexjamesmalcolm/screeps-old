7970fe5237dbea6ace24ab2d7087ee9631de1e17
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-08 02:51:26 -0400

    Update Room.findAll.js

--- Room.findAll.js
+++ Room.findAll.js
@@ -2,6 +2,7 @@ var RoomFindAll = function() {
     var found = {};
     found.flags = this.find(FIND_FLAGS);
     found.sources = this.find(FIND_SOURCES);
+    found.activeSources = this.find(FIND_ACTIVE_SOURCES);
     found.myCreeps = this.find(FIND_MY_CREEPS);
     found.myConstructionSites = this.find(FIND_MY_CONSTRUCTION_SITES);
     found.mySpawns = this.find(FIND_MY_SPAWNS);
