a8a6f08666437b9afdbbcf85a40c77fe55639751
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-09 12:18:19 -0400

    Update Creep.getRepositories.js

--- Creep.getRepositories.js
+++ Creep.getRepositories.js
@@ -1,5 +1,21 @@
 var CreepGetRepositories = function() {
-    console.log();
+    var foundStructures = this.room.found.structures;
+    var inRangeStructures = this.pos.findInRange(foundStructures, 1);
+    var repositories = _.filter(inRangeStructures, function(structure) {
+        var viableRepositories = [
+            STRUCTURE_SPAWN,
+            STRUCTURE_EXTENSION,
+            STRUCTURE_LINK,
+            STRUCTURE_STORAGE,
+            STRUCTURE_TOWER,
+            STRUCTURE_TERMINAL,
+            STRUCTURE_CONTAINER
+        ];
+        if(viableRepositories.indexOf(structure.structureType) > -1) {
+            return true;
+        }
+    });
+    return repositories;
 };
 
 module.exports = CreepGetRepositories
