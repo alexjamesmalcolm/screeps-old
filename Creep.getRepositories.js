55ee69725e467fa87f8028229c80ac28e8658b38
Alex Malcolm <alexjamesmalcolm@gmail.com>
2017-05-12 05:04:34 -0400

    Update Creep.getRepositories.js

--- Creep.getRepositories.js
+++ Creep.getRepositories.js
@@ -1,24 +1,30 @@
-var CreepGetRepositories = function() {
+var CreepGetRepositories = function(blacklist) {
+    if(!blacklist) {
+        blacklist = [];
+    }
     var foundStructures = this.room.memory.found.structures;
     var inRangeStructures = this.pos.findInRange(foundStructures, 1);
+    var viableRepositories = [
+        STRUCTURE_SPAWN,
+        STRUCTURE_EXTENSION,
+        STRUCTURE_LINK,
+        STRUCTURE_STORAGE,
+        STRUCTURE_TOWER,
+        STRUCTURE_CONTAINER
+    ];
+    
     var repositories = _.filter(inRangeStructures, function(structure) {
         var type = structure.structureType
         var amount;
-        var viableRepositories = [
-            STRUCTURE_SPAWN,
-            STRUCTURE_EXTENSION,
-            STRUCTURE_LINK,
-            STRUCTURE_STORAGE,
-            STRUCTURE_TOWER,
-            STRUCTURE_CONTAINER
-        ];
-        if(viableRepositories.indexOf(type) > -1) {
-            if(type === STRUCTURE_STORAGE || type === STRUCTURE_CONTAINER) {
-                amount = structure.storeCapacity - structure.store.energy;
-            } else {
-                amount = structure.energyCapacity - structure.energy;
+        if(blacklist.indexOf(type) === -1) {
+            if(viableRepositories.indexOf(type) > -1) {
+                if(type === STRUCTURE_STORAGE || type === STRUCTURE_CONTAINER) {
+                    amount = structure.storeCapacity - structure.store.energy;
+                } else {
+                    amount = structure.energyCapacity - structure.energy;
+                }
+                return amount > 0;
             }
-            return amount > 0;
         }
     });
     return repositories;
